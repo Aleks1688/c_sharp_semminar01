0d42d642aaa34742e76c8b5319f5727eb1b050b9
Sasha <sfh1688@qq.com>
2024-02-17 16:55:28 +0800

    edit Semminar01:Task01

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,6 +1,12 @@
 ﻿Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32(Console.ReadLine());
 
+if (num <= 0)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return 546456;
+}
+
 int count = -num;
 
 while (count <= num)
