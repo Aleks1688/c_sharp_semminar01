ac228728a5eb103ca23fd4163b3241e4cd612b5c
Sasha <sfh1688@qq.com>
2024-02-17 16:28:18 +0800

    semminar:Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Compression;
+
+Console.WriteLine("Введите целое число a");
+string str = Console.ReadLine();
+int num1 = Convert.ToInt32(str);
+
+Console.WriteLine("Введите целое число b");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = num1 == num2 * num2;
+
+string resultStr = result ? "да" : "нет";
+
+Console.WriteLine($"a = {num1}, b = {num2} => {resultStr}");
