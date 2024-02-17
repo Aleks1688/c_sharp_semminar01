af46429328a3a58b0fd7dcec75baffcad8ab1dc3
Sasha <sfh1688@qq.com>
2024-02-17 16:43:50 +0800

    Semminar01:Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = -num;
+
+while (count <= num)
+{
+    //Console.Write(count + " ");
+    Console.Write($"{count} ");
+    count++;
+}
