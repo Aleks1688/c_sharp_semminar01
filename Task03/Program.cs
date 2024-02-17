c6af2791a619b8c6729cafb974f624b41920eb7d
Sasha <sfh1688@qq.com>
2024-02-17 17:22:50 +0800

    add semminar01:Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите трехзначное целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numCheck = num;
+if (num <0 ) numCheck = -num;
+
+if(numCheck >= 100 && numCheck <= 999)
+{
+    int digitFirst = numCheck / 100;
+    int digitLast = numCheck % 10;
+
+    int sumDigits = digitFirst + digitLast;
+    Console.WriteLine($"сумма первой и последней цифры = {sumDigits}");
+}
+else
+{
+    Console.WriteLine("некорректный ввод");
+}
